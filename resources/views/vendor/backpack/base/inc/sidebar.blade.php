1ea3c9e9b26af7ecd19d4a8d5aa6587746e4a3ff
Master_PC <mengeang.it@gmail.com>
2017-08-23 10:06:51 +0700

    side bar

--- resources/views/vendor/backpack/base/inc/sidebar.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar.blade.php
@@ -49,7 +49,7 @@
           <li class="treeview">
             <a href="#">
               <i class="fa fa-university"></i>
-              <span>Item</span>
+              <span>Product</span>
               <span class="pull-right-container">
                   <i class="fa fa-angle-left pull-right"></i>
                     </span>
